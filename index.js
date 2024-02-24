ff5f45bbac49c4def0d3293fb8e1389e802162e1
An Tran <anth24@fpt.com>
2024-02-23 23:40:02 -0500

    Change path

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 app.set('view engine', 'ejs')
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(__dirname+ "/public"));
 app.set('views', __dirname + '/views')
 
 const port = process.env.PORT || 3000;
